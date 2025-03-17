ec5cfc6266fd45fa79a10798a5ab782b93ca451a
Felgi <felipeferraridoval@gmail.com>
2025-03-17 12:13:57 -0300

    Tipos de variables

---
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeclaracionDeVariables : MonoBehaviour
+{
+    [SerializeField] private int cantCiudades;
+    [SerializeField] private float kilometros;
+    [SerializeField] private char inicial;
+    [SerializeField] private string nombre;
+    [SerializeField] private bool presente;
+
+    void Start()
+    {
+        cantCiudades = 2;
+        kilometros = 10.2f;
+        inicial = 'F';
+        nombre = "Felipe";
+        presente = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Entero " + cantCiudades);
+        Debug.Log("Float " + kilometros);
+        Debug.Log("Caracter " + inicial);
+        Debug.Log("String " + nombre);
+        Debug.Log("Booleano " + presente);
+    }
+}
