ea2e57103deab1a23fbb298dab9b7d583d9671ef
Felgi <felipeferraridoval@gmail.com>
2025-03-17 11:36:57 -0300

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float Horizontal;
+    [SerializeField] private float Vertical;
+    void Start()
+    {
+        Horizontal = Input.GetAxis("Horizontal");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
